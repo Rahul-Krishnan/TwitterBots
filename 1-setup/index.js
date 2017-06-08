050d2e8823b7d8869ddc9f9b30942b874c9a8aec
Rahul Krishnan <rk2211@gmail.com>
2017-06-07 21:28:22 -0400

    Interacted with users

--- 1-setup/index.js
+++ 1-setup/index.js
@@ -9,10 +9,60 @@ let bot = new Twit({
   timeout_ms: 60*1000
 });
 
-bot.post('statuses/update', {status: 'Who\'s afraid of the big bad wolf?'}, function(err, data, response){
-  if (err){
+// Post a status update:
+// bot.post('statuses/update', {status: 'Who\'s afraid of the big bad wolf?'}, function(err, data, response){
+//   if (err){
+//     console.log(err);
+//   } else {
+//     console.log(data.text + ' was tweeted.');
+//   }
+// });
+
+// Get followers' screen names:
+// bot.get('followers/list', {screen_name: 'Rahul_Dev_Acct'}, function(err, data, response){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     data.users.forEach(function(user) {
+//       console.log(user.screen_name);
+//     });
+//   }
+// });
+
+// Follow a user:
+// bot.post('friendships/create', {screen_name: 'theanswerismud'}, function(err, data, response){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(data);
+//   }
+// });
+
+// Get list of accounts bot is following:
+// bot.get('friends/list', {screen_name: 'Rahul_Dev_Acct'}, function(err, data, response){
+//   if (err){
+//     console.log(err);
+//   } else {
+//     data.users.forEach(function(user) {
+//       console.log(user.screen_name);
+//     });
+//   }
+// });
+
+// Check if bot is following/being followed by a user:
+// bot.get('friendships/lookup', {screen_name: 'theanswerismud'}, function(err, data, response){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(data);
+//   }
+// });
+
+// Send a DM to a user:
+bot.post('direct_messages/new', {screen_name: 'theanswerismud', text: 'Hello Rahul! This is Robot Rahul!'}, function(err, data, response){
+  if (err) {
     console.log(err);
   } else {
-    console.log(data.text + ' was tweeted.');
+    console.log(data);
   }
 });
