fd1267390cf6440ba5cd330b6e6f66b2dfac759a
Rahul Krishnan <rk2211@gmail.com>
2017-06-18 21:38:53 -0400

    add tracery

--- 10-story-generation-bot/index.js
+++ 10-story-generation-bot/index.js
@@ -1,6 +1,6 @@
 /* jshint esversion: 6 */
 let Twit = require('twit');
-
+let tracery = require('tracery-grammar');
 
 let bot = new Twit({
   consumer_key: process.env.LEARNINGBOT_CONSUMER_KEY,
@@ -8,4 +8,8 @@ let bot = new Twit({
   access_token: process.env.LEARNINGBOT_ACCESS_TOKEN,
   access_token_secret: process.env.LEARNINGBOT_ACCESS_TOKEN_SECRET,
   timeout_ms: 60*1000
+});
+
+let grammar = tracery.createGrammar({
+  'character': ['K', 't'],
 });
