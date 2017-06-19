0e358e2d1b7f51a648f73ef64375fac992cd56f0
Rahul Krishnan <rk2211@gmail.com>
2017-06-19 02:47:30 -0400

    Tweet from a spreadsheet

--- 11-google-spreadsheets-bot/index.js
+++ 11-google-spreadsheets-bot/index.js
@@ -1,5 +1,6 @@
 /* jshint esversion: 6 */
 let Twit = require('twit');
+let Tabletop = require('tabletop');
 
 
 let bot = new Twit({
@@ -8,4 +9,24 @@ let bot = new Twit({
   access_token: process.env.LEARNINGBOT_ACCESS_TOKEN,
   access_token_secret: process.env.LEARNINGBOT_ACCESS_TOKEN_SECRET,
   timeout_ms: 60*1000
+});
+
+let spreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1OKD_n-g8JTxQVQnTeMqpQwHDDZ1R5FDlGnYz_IETLks/pubhtml';
+
+Tabletop.init({
+  key: spreadsheetUrl,
+  callback: function(data, tabletop){
+    console.log(data);
+    data.forEach(function(d){
+      let status = d.Name + ' is a great name for a Twitter bot!';
+      bot.post('statuses/update', {status: status}, function(err, response, data){
+        if (err){
+          console.log(err);
+        } else {
+          console.log('Posted!');
+        }
+      });
+    });
+  },
+  simpleSheet: true
 });
