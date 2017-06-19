2baa58933aba8e5667230f93f788c148c0d0c743
Rahul Krishnan <rk2211@gmail.com>
2017-06-19 02:18:37 -0400

    Bot set up for story tweeting

--- 10-story-generation-bot/index.js
+++ 10-story-generation-bot/index.js
@@ -11,5 +11,32 @@ let bot = new Twit({
 });
 
 let grammar = tracery.createGrammar({
-  'character': ['K', 't'],
+  'character': ['Karl', 'Aida', 'Hans'],
+  'place': ['office', 'bank', 'court'],
+  'object': ['letter', 'paper', 'bribe'],
+  'setPronouns': [
+    '[they:they][them:them][their:their][theirs:theirs]',
+    '[they:she][them:her][their:her][theirs:hers]',
+    '[they:he][them:him][their:his][theirs:his]'
+  ],
+  'setJob': [
+    '[job:lawyer][actions:argued in court,filed some paperwork]',
+    '[job:inspector][actions:talked with the lawyer,conducted meetings]',
+    '[job:officer][actions:arrested people,stood in the courtroom]'
+  ],
+  'story': ['#protagonist# the #job# went to the #place# every day. Usually #they# #actions#. Then #they# picked up #their# #object#.'],
+  'origin': ['#[#setPronouns#][#setJob#][protagonist:#character#]story#']
+});
+
+grammar.addModifiers(tracery.baseEngModifiers);
+
+let story = grammar.flatten('#origin#');
+console.log(story);
+
+bot.post('statuses/update', {status: story}, function(err, data, response){
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Bot has tweeted '+story);
+  }
 });
